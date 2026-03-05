8ea0f400475c5b2603e8215cd8d6facfd9c3cf89
Manondev11 <manon.elm@hotmail.com>
2026-03-05 16:06:25 +0100

    maj

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,79 +10,72 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/user')]
 final class UserController extends AbstractController
 {
-    #[Route('/admin/user', name: 'app_user')]
-    #[IsGranted('ROLE_ADMIN')]
+    #[Route(name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
-
-        $users = $userRepository->findAll();
         return $this->render('user/index.html.twig', [
-            // 'controller_name' => 'UserController',
-            'users' => $users
+            'users' => $userRepository->findAll(),
         ]);
     }
-    #[Route('/admin/user/{id}/to/editor', name: 'app_user_to_editor')]
-    public function changeRoleToEditor(EntityManagerInterface $entityManager, User $user): Response
-    {
-        $user->setRoles(['ROLE_EDITOR', 'ROLE_USER']);
-        $entityManager->flush();
 
-        $this->addFlash('success', 'Le rôle de l\'utilisateur a été changé avec succès.');
-        return $this->redirectToRoute('app_user');
-    }
-    #[Route('/admin/user/{id}/change-role/{role}', name: 'app_user_change_role')]
-    public function changeRole(EntityManagerInterface $entityManager, User $user, $role): Response
+    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $validRoles = ['ROLE_EDITOR', 'ROLE_USER'];
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
-        if (!in_array($role, $validRoles, true)) {
-            $this->addFlash('error', "Le rôle demandé n'est pas valide.");
-            return $this->redirectToRoute('app_user');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        if ($role !== 'ROLE_USER') {
-            $user->setRoles([$role, 'ROLE_USER']);
-        } else {
-            $user->setRoles([$role]);
-        }
-        $entityManager->flush();
-
-        $this->addFlash('success', "Le rôle $role a bien été attribué à l'utilisateur.");
-
-        return $this->redirectToRoute('app_user');
+        return $this->render('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
     }
-    #[Route('/admin/user/{id}/remove/editor/role ', name: 'app_user_remove_editor_role')]
-    public function removeRoleEditor(EntityManagerInterface $entityManager, User $user): Response
-    {
-        $user->setRoles([]);
-        $entityManager->flush();
 
-        $this->addFlash('danger', "Le rôle éditeur à bien été retiré à l'utilisateur");
-
-        return $this->redirectToRoute('app_user');
-    }
-    #[Route('/admin/user/{id}', name: 'app_user_show')]
+    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
     public function show(User $user): Response
     {
         return $this->render('user/show.html.twig', [
             'user' => $user,
         ]);
     }
-    
-    #[Route('/admin/user/{id}/remove/', name: 'app_user_remove')]
-    public function ruserRemove(EntityManagerInterface $entityManager, $id,  UserRepository $userRepository): Response
+
+    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
-        $user = $userRepository->find($id);
-        $entityManager->remove($user);
-        $entityManager->flush();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
-        $this->addFlash('danger', "L'utilisateur à bien été supprimé.");
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
 
-        return $this->redirectToRoute('app_user');
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
     }
 }
